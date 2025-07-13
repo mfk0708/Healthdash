8076b60cd9117c9fa41b4f0c267358183b2af2c9
mfk0708 <mfahadkhan8010@gmail.com>
2025-07-13 19:23:04 +0530

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-    theme: {
-      extend: {},
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+       borderColorCustom: '#e5e7eb'
+      },
     },
-    plugins: [],
-  };
+  },
+  plugins: [],
+};
